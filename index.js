8b0d62c7aa0700a64212408673a49ebd269ca01e
GennadyVenidiktov <wenter3@outlook.com>
2021-03-06 03:59:30 +0500

    Бургер меню готово

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+let buttonBurger = document.querySelector('button.navbar-toggler_open');
+let buttonClose = document.querySelector('button.navbar-toggler_close');
+
+buttonBurger.addEventListener('click', () => {
+	document.querySelector('.logo_start').classList.toggle("disable");
+	document.querySelector('.logo_next').classList.toggle("disable");
+	document.querySelector('nav.navbar').classList.toggle("backColor");
+
+	document.querySelector('button.navbar-toggler_open').classList.toggle("disable");
+	document.querySelector('button.navbar-toggler_close').classList.toggle("disable");
+})
+
+buttonClose.addEventListener('click', () => {
+	document.querySelector('nav.navbar').classList.toggle("backColor");
+	document.querySelector('button.navbar-toggler_open').classList.toggle("disable");
+	document.querySelector('button.navbar-toggler_close').classList.toggle("disable");
+	document.querySelector('.logo_start').classList.toggle("disable");
+	document.querySelector('.logo_next').classList.toggle("disable");
+
+
+})
